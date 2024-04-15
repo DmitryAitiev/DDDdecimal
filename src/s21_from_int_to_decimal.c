bf2b5cab8afaadd4f7124d69978dd279fa7cd10b
Tsuboi Karthwasten <tsuboika@su-c4.msk.21-school.ru>
2024-04-15 23:47:07 +0300

    int_to_dec

--- src/s21_from_int_to_decimal.c
+++ src/s21_from_int_to_decimal.c
@@ -2,19 +2,30 @@
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
   int code = 0;
-  if (!dst) code = 1;
-  if (src < 0)
+  int flag = 0;
+  if (!dst) {
+    return 1;
+  }
+  if (src < 0) {
     dst->bits[3] = 0b10000000000000000000000000000000;
-  else dst->bits[3] = 0b00000000000000000000000000000000;
-  dst->bits[2] = 0b00000000000000000000000000000000;
-  dst->bits[1] = 0b00000000000000000000000000000000;
+  } else {
+    dst->bits[3] = 0b00000000000000000000000000000000;
+  }
   dst->bits[0] = 0b00000000000000000000000000000000;
-  src = abs(src);
-  for (int i = 31; i > 0&&src > 0; i--) {
-    if (src / (int)pow(2, i) > 0) {
-      dst->bits[0] = (dst->bits[0] | 0b00000000000000000000000000000001 << i);
+  dst->bits[1] = 0b00000000000000000000000000000000;
+  dst->bits[2] = 0b00000000000000000000000000000000;
+  if(src == -2147483648){
+    src = abs(src + 1);
+    flag = 1;
+  }
+  else src = abs(src);
+  for (int i = 31; i >= 0 && src > 0; i--) {
+    if ((src / (int)pow(2, i)) > 0) {
+      dst->bits[0] = (dst->bits[0] | (0b00000000000000000000000000000001 << i));
       src -= (int)pow(2, i);
     }
   }
+  if(flag)
+    dst->bits[0]=dst->bits[0]+1;
   return code;
 }
