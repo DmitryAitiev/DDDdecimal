806fc8dfc0c2e69ece613b08984feca04fe469c6
Tsuboi Karthwasten <tsuboika@su-c4.msk.21-school.ru>
2024-04-15 21:07:15 +0300

    d_to_f

--- src/s21_from_decimal_to_float.c
+++ src/s21_from_decimal_to_float.c
@@ -2,26 +2,24 @@
 
 int s21_from_decimal_to_float(s21_decimal src, float *dst){
   int code =0, scale=get_scale(src);
-if (scale>6) code=1;
-long double result;
+double result = 0;
 
-for(int i = 95; i >= 64; i--){
-  if (_get_bit_int(i, 2))
- result += pow(2, i);
-}
+  result += src.bits[0];
 
 for(int i = 63; i >= 32; i--){
-  if (_get_bit_int(i, 1))
+  if (_get_bit_int(src.bits[1], i))
  result += pow(2, i);
 }
 
-for(int i = 31; i >= 0; i--){
-  if (_get_bit_int(i, 0))
+for(int i = 95; i >= 64; i--){
+  if (_get_bit_int(src.bits[2], i))
  result += pow(2, i);
 }
 
 float res=(float)result/pow(10,scale);
-if (get_sign(src)) res=res*(-1);
+if (get_sign(src)) 
+  res=res*(-1);
 *dst = res;
 return code;
 }
+
